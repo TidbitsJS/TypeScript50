626a2e831299b353cba9b585e8932eeaebf55017
Tidbits JS <tidbitsjs@gmail.com>
2022-08-18 13:10:36 +0530

    display & delete items

--- mosh08/src/App.tsx
+++ mosh08/src/App.tsx
@@ -1,22 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import ReminderList from "./components/ReminderList";
 import Reminder from "./types/Reminder";
+import ReminderService from "./services/reminder";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [reminders, setReminder] = useState<Reminder[]>([
-    {
-      id: 1,
-      title: "Buy milk",
-    },
-  ]);
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await ReminderService.getReminders();
+    setReminders(reminders);
+  };
+
+  const removeReminder = (id: number) => {
+    const result = reminders.filter((reminder) => reminder.id !== id);
+    setReminders(result);
+  };
 
   return (
     <div className="App">
-      <ReminderList items={reminders} />
+      <ReminderList items={reminders} onRemoveReminder={removeReminder} />
     </div>
   );
 }
--- mosh08/src/components/ReminderList.tsx
+++ mosh08/src/components/ReminderList.tsx
@@ -2,13 +2,22 @@ import Reminder from "../types/Reminder";
 
 interface ReminderListProps {
   items: Reminder[];
+  onRemoveReminder: (id: number) => void;
 }
 
-const ReminderList = ({ items }: ReminderListProps) => {
+const ReminderList = ({ items, onRemoveReminder }: ReminderListProps) => {
   return (
-    <ul>
+    <ul className="list-group">
       {items.map((item) => (
-        <li key={item.id}>{item.title}</li>
+        <li key={item.id} className="list-group-item">
+          {item.title}
+          <button
+            onClick={() => onRemoveReminder(item.id)}
+            className="btn btn-outline-danger mx-2 rounded-pill"
+          >
+            Delete
+          </button>
+        </li>
       ))}
     </ul>
   );
